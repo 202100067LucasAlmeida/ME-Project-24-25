44f07b6a920f13cfb83e7b78bdfd405ae78622d0
202100067LucasAlmeida <202100067@estudantes.ips.pt>
2025-03-24 15:16:35 +0000

    .

--- Trabalho_ME.R
+++ Trabalho_ME.R
@@ -1,8 +1,50 @@
 ##########################
+######              ######
 ##### Trabalho de ME #####
+######              ######
 ##########################
 
+# Alunos:
+#
 # Lucas Almeida - 202100067
 # Diana Francisco - 202100673
 # Rita Pereira - 202200170
 
+# Docente:
+#
+# Profª Vanda Rosado
+
+###################
+######       ######
+##### 1° Fase #####
+######       ######
+###################
+
+# Recolha da base de dados
+dados <- read.csv("D:/Lucas Alexandre/IPS/ME-Project-24-25/data.csv", sep= ";", header = TRUE)
+# dados <- read.csv("caminho-pc-diana", sep=";", header = TRUE)
+# dados <- read.csv("caminho-pc-rita", sep=";", header = TRUE)
+
+#######################
+###                 ###
+## Análise dos dados ##
+###                 ###
+#######################
+
+# Nosso trabalho irá abordar um estudo do desempenho dos alunos em matemática 
+# no ensino secundário de duas escolas portuguesas.
+
+# População:
+# Alunos do ensino secundário de duas escolas portuguesas.
+#
+# Unidade Estatística: Alunos
+
+
+# Amostra: 395 alunos (de duas escolas diferentes)
+amostra <- nrow(dados)
+
+########################################
+###                                  ###
+## Variáveis estatísticas para estudo ##
+###                                  ###
+########################################
